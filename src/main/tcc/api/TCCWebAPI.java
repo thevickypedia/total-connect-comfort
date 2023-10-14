47bc535dfd251cd40e3cc6422a9f15c24599bd12
thevickypedia <vignesh.sivanandarao@gmail.com>
2023-10-14 09:01:13 -0500

    Finally, get the right HTML response Have a method to convert JSON to String

--- src/main/tcc/api/TCCWebAPI.java
+++ src/main/tcc/api/TCCWebAPI.java
@@ -11,14 +11,12 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.LaxRedirectStrategy;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ConnectException;
-import java.net.HttpURLConnection;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Logger;
@@ -34,40 +32,25 @@ public class TCCWebAPI {
         logger.addHandler(handler);
     }
 
-    public static String getResponse(HttpURLConnection connection) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        StringBuilder stringify = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            stringify.append(line);
-        }
-        connection.disconnect();
-        return stringify.toString();
-    }
-
-    public static boolean validate_auth_response(HttpResponse response, String response_url)
+    public static boolean validate_auth_response(String responseBody, String response_url)
             throws AuthenticationError, TooManyAttemptsError, RedirectError, LoginUnexpectedError {
         logger.info(response_url);
-        try {
-            String responseBody = EntityUtils.toString(response.getEntity());
-            logger.info(responseBody);
-            if (responseBody.contains("The email or password provided is incorrect") | responseBody.contains("The email address is not in the correct format")) {
-                throw new AuthenticationError(String.format("Email (%s) and/or password not accepted", settings.username));
-            }
-            if (response_url.contains("TooManyAttempts")) {
-                throw new TooManyAttemptsError("Too many attempts");
-            }
-            if (!response_url.contains("portal/")) {
-                throw new RedirectError("Couldn't redirect to portal page");
-            }
-            if (response_url.contains("/Error")) {
-                throw new LoginUnexpectedError("Unexpected login error received");
-            }
-            return true;
-        } catch (IOException error) {
-            logger.warning(error.toString());
-            return false;
+        if (responseBody.contains("The email or password provided is incorrect") |
+                responseBody.contains("The email address is not in the correct format") |
+                responseBody.contains("Login was unsuccessful.")) {
+            throw new AuthenticationError(String.format("Email (%s) and/or password not accepted", settings.username));
         }
+        if (response_url.contains("TooManyAttempts")) {
+            throw new TooManyAttemptsError("Too many attempts");
+        }
+        if (!response_url.contains("portal/") && !responseBody.contains("/portal/Device/Alerts?locationId=")) {
+            throw new RedirectError("Couldn't redirect to portal page");
+        }
+        if (response_url.contains("/Error")) {
+            throw new LoginUnexpectedError("Unexpected login error received");
+        }
+        System.out.println("responseBody has to be further processed..");
+        return true;
     }
 
     public static String stringifyJSON(JSONObject data, boolean newline) {
@@ -94,31 +77,21 @@ public class TCCWebAPI {
     }
 
     public static boolean do_authenticate() throws AuthenticationError, TooManyAttemptsError, RedirectError, LoginUnexpectedError {
-        // Create an HTTP client
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-
-        // Create an HTTP POST request
+        CloseableHttpClient httpClient = HttpClients.custom().setRedirectStrategy(new LaxRedirectStrategy()).build();
         HttpPost httpPost = new HttpPost(settings.base_url);
-
         try {
-            // Set the request parameters
-            String jsonRequest = "{\"UserName\":\"" + settings.username + "\",\"Password\":\"" + settings.password + "\"}";
+            JSONObject data = new JSONObject();
+            data.put("UserName", settings.username);
+            data.put("Password", settings.password);
+            String jsonRequest = stringifyJSON(data, false);
             StringEntity requestEntity = new StringEntity(jsonRequest);
             requestEntity.setContentType("application/json");
             httpPost.setEntity(requestEntity);
-
-            // Execute the POST request
             HttpResponse response = httpClient.execute(httpPost);
-
-            // Get the response content
-            String responseBody = EntityUtils.toString(response.getEntity());
-
-            System.out.println("Response Status Code: " + response.getStatusLine().getStatusCode());
-            System.out.println("Response Body: " + responseBody);
-
             int responseCode = response.getStatusLine().getStatusCode();
+            String responseBody = EntityUtils.toString(response.getEntity());
             if (responseCode <= 400) {
-                boolean validated = validate_auth_response(response, httpPost.getURI().toString());
+                boolean validated = validate_auth_response(responseBody, httpPost.getURI().toString());
                 try {
                     httpClient.close();
                 } catch (Exception error) {
@@ -141,7 +114,6 @@ public class TCCWebAPI {
                 if (do_authenticate()) {
                     return;
                 } else {
-                    // todo: check how to proceed
                     System.out.println("Unexpected");
                     break;
                 }
