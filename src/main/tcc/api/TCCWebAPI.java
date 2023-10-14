a6cc0618c8f3886b2a80e5af8cefd3b9b49e92cb
thevickypedia <vignesh.sivanandarao@gmail.com>
2023-10-13 22:58:46 -0500

    Instantiate TCCWebAPI

--- src/main/tcc/api/TCCWebAPI.java
+++ src/main/tcc/api/TCCWebAPI.java
@@ -1,4 +1,34 @@
 package main.tcc.api;
 
+import main.tcc.LoggingFormatter;
+
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Logger;
+
+
 public class TCCWebAPI {
+    public static Logger logger = Logger.getLogger(TCCWebAPI.class.getName());
+
+    public static void setupLogger() {
+        logger.setUseParentHandlers(false);
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setFormatter(new LoggingFormatter());
+        logger.addHandler(handler);
+    }
+
+    public static void authenticate() throws InterruptedException {
+        for (int i = 1; i < 31; i++) {
+            logger.info("Starting authentication attempt " + i);
+            System.out.println("Do AUTH");
+        }
+    }
+
+    public static void main(String[] args) {
+        setupLogger();
+        try {
+            authenticate();
+        } catch (InterruptedException error) {
+            logger.info(error.toString());
+        }
+    }
 }
