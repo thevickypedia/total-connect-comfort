9c8b210fc0a0107218a46bf1998775deb011e92e
thevickypedia <vignesh.sivanandarao@gmail.com>
2023-10-13 22:57:37 -0500

    Add custom exceptions

---
+++ src/main/tcc/api/exceptions.java
@@ -0,0 +1,34 @@
+package main.tcc.api;
+
+public class exceptions {
+    public static class TCCWebAPIError extends Exception {
+        public TCCWebAPIError(String message) {
+            super(message);
+        }
+    }
+
+    public static class AuthenticationError extends TCCWebAPIError {
+        public AuthenticationError(String message) {
+            super(message);
+        }
+    }
+
+    public static class TooManyAttemptsError extends TCCWebAPIError {
+        public TooManyAttemptsError(String message) {
+            super(message);
+        }
+    }
+
+    public static class RedirectError extends TCCWebAPIError {
+        public RedirectError(String message) {
+            super(message);
+        }
+    }
+
+    public static class LoginUnexpectedError extends TCCWebAPIError {
+        public LoginUnexpectedError(String message) {
+            super(message);
+        }
+    }
+
+}
